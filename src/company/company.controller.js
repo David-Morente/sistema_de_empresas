8c54acb760c152bd65267d128e630626022b9d86
David-Morente <davidmorenteg07@gmail.com>
2025-02-26 14:12:25 -0600

    Registro de empresa creado

---
+++ src/company/company.controller.js
@@ -0,0 +1,21 @@
+import Companys from "./company.model.js"
+
+export const registerCompany = async (req, res) => {
+    try {
+        const data = req.body
+
+        const company = await Companys.create(data)
+        return res.status(201).json({
+            message: "Empresa registrada",
+            company
+        })
+
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).json({
+            message: "Fallo al registrar la empresa",
+            error: err.message
+        })
+    }
+}
+
